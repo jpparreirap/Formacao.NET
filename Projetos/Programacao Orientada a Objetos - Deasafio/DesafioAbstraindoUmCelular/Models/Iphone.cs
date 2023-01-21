9e1354ae616ee3fbaa6f452763197c5d965aae83
jpparreirap <jpparreirap@gmail.com>
2023-01-21 16:58:57 -0300

    Desafio POO

---
+++ Projetos/Programacao Orientada a Objetos - Deasafio/DesafioAbstraindoUmCelular/Models/Iphone.cs
@@ -0,0 +1,14 @@
+namespace DesafioAbstraindoUmCelular.Models
+{
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o {nomeApp} no Iphone");
+        }
+    }
+}
---
+++ Projetos/Programacao Orientada a Objetos - Deasafio/DesafioAbstraindoUmCelular/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioAbstraindoUmCelular.Models;
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "1234567", modelo: "Modelo 1", imei: "1111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "7654321", modelo: "Modelo 2", imei: "2222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
